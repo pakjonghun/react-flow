ac0ed7a325693e1d83c768e49e4cd015e4e027f8
박정훈 <fireking5997@famoz.co.kr>
2022-11-21 17:52:29 +0900

    custom node

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,80 @@
-import React from "react";
-import ReactFlow, { useNodesState } from "reactflow";
+import React, { useCallback, useEffect } from "react";
+import ReactFlow, {
+  Position,
+  useEdges,
+  useEdgesState,
+  useNodesState,
+  addEdge,
+} from "reactflow";
 import styled from "styled-components";
 import Color from "./Color";
 
+const nodeTypes = {
+  selectorNode: Color,
+};
+
+const initNodes = [
+  {
+    id: "1",
+    position: {
+      x: 100,
+      y: 100,
+    },
+    data: {
+      label: "hi",
+    },
+    sourcePosition: Position.Right,
+    targetPosition: Position.Left,
+  },
+  {
+    id: "2",
+    position: {
+      x: 200,
+      y: 200,
+    },
+    data: {
+      label: "hi",
+    },
+    type: "selectorNode",
+  },
+];
+
 const App = () => {
-  const [nodes, setNodes, onNodeChange] = useNodesState([]);
-  const [edges, setEdges, onEdgesChange] = useNodesState([]);
-  const nodeTypes = {
-    selectorNode: Color,
-  };
+  const [nodes, setNodes, onNodeChange] = useNodesState(initNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+
+  useEffect(() => {
+    const onChange = (event: any) => {
+      setNodes((nds) => {
+        return nds.map((node) => {
+          if (node.id === "2") {
+            return node;
+          }
+
+          return { ...node, data: node.data, color: "gray" };
+        });
+      });
+    };
+  }, []);
+
+  const onConnect = useCallback(
+    (params: any) =>
+      setEdges((eds) =>
+        addEdge({ ...params, animated: true, style: { stroke: "#fff" } }, eds)
+      ),
+    []
+  );
 
   return (
     <Container>
-      <ReactFlow />
+      <ReactFlow
+        onNodesChange={onNodeChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodes={nodes}
+        edges={edges}
+        nodeTypes={nodeTypes}
+      />
     </Container>
   );
 };
--- src/Color.tsx
+++ src/Color.tsx
@@ -1,32 +1,34 @@
 import { Handle, Position } from "reactflow";
 
 const Color = () => {
-  <>
-    <Handle
-      type="target"
-      position={Position.Left}
-      style={{ backgroundColor: "red" }}
-      onConnect={(param) => console.log("on connect param", param)}
-    />
-    <div>custom</div>
-    <Handle
-      type="source"
-      position={Position.Right}
-      style={{ backgroundColor: "blue" }}
-      onConnect={(param) => console.log("on connect param", param)}
-      isConnectable={false}
-      id="a"
-    />
+  return (
+    <>
+      <Handle
+        type="target"
+        position={Position.Left}
+        style={{ backgroundColor: "red" }}
+        onConnect={(param) => console.log("on connect param", param)}
+      />
+      <div>custom</div>
+      <Handle
+        type="source"
+        position={Position.Right}
+        style={{ backgroundColor: "blue" }}
+        onConnect={(param) => console.log("on connect param", param)}
+        isConnectable={false}
+        id="a"
+      />
 
-    <Handle
-      type="source"
-      position={Position.Right}
-      style={{ backgroundColor: "skyBlue" }}
-      onConnect={(param) => console.log("on connect param", param)}
-      isConnectable
-      id="b"
-    />
-  </>;
+      <Handle
+        type="source"
+        position={Position.Right}
+        style={{ backgroundColor: "skyBlue" }}
+        onConnect={(param) => console.log("on connect param", param)}
+        isConnectable
+        id="b"
+      />
+    </>
+  );
 };
 
 export default Color;
